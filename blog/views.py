f0363b236adc961e5b3bd02615bb4c2e713d3523
Kadyrgali <galishka15@gmail.com>
2020-11-05 16:03:35 +0600

    Added

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,16 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    for i in range(len(posts)):
+        if i%2==0:
+            posts[i].even = True
+        else:
+            posts[i].even = False
+            
     return render(request, 'blog/post_list.html', {'posts': posts})
-    Post.objects.get(pk=pk)
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
